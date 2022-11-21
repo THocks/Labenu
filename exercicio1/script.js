d6f037a077f57b92c4d71af1053bb100d2e8e1d3
Thiago De Lima Batista <102311684+THocks@users.noreply.github.com>
2022-11-21 16:28:03 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,34 @@
+/*let nome 
+let idade 
+nome = prompt("Qual seu nome ?")
+idade = prompt('Qual sua idade ?')
+
+alert(`Olá ${nome} você tem ${idade} anos`)
+
+*/
+
+
+// Parte 2
+
+/*
+let anime 
+anime = prompt('Qual seu anime favorito ')
+let idade 
+idade = parseInt(prompt ('Qual sua idade '))
+let cidade 
+cidade = prompt('Qual sua cidade ')
+
+
+
+
+alert(`O seu anime favorito è ${anime} \n você tem ${idade} \n e mora na cidade de ${cidade}`)
+
+*/
+
+
+
+
+
+
+
+
